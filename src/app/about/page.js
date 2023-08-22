169a107252e56958da252b5820b3ff4e67645378
Vishal__Dev <mylaptop800mrj@gmail.com>
2023-08-22 13:22:48 +0530

    intro about routng nodejs support folder base routing

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import styles from '../page.module.css'
+
+const AboutComponent = ()=>{
+    return (
+        <main className={styles.main}>
+            <h1>About page | routing</h1>
+        </main>
+    )
+}
+
+export default AboutComponent;
---
+++ src/app/login/page.js
@@ -0,0 +1,11 @@
+import styles from '../page.module.css'
+
+const page = () => {
+  return (
+    <main className={styles.main}>
+        <h1>Basic Routing | Login Page</h1>
+    </main>
+  )
+}
+
+export default page
