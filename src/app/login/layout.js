9abbedda5a5cc53a33f34afd265a184f9c187a59
Vishal__Dev <mylaptop800mrj@gmail.com>
2023-08-29 16:47:00 +0530

    conditiona layout in login teacher layout.js

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,21 +1,28 @@
+"use client"
+
 import Link from "next/link";
 import './login.css'
+import { usePathname } from "next/navigation";
 
 export default function Layout({children}) {
+    const pathName = usePathname();
+    console.log(pathName);
   return (
     <div>
-        <ul className="login-list">
-        <h2>Login Navigation</h2>
-            <li>
-                <Link href={'/login'}>Login</Link>
-            </li>
-            <li>
-                <Link href={'/login/loginStudents'}>Login Students</Link>
-            </li>
-            <li>
-                <Link href={'/login/loginTeacher'}>Login Teacher</Link>
-            </li>
-        </ul>
+        {
+            pathName !== '/login/loginTeacher'?<ul className="login-list">
+            <h2>Login Navigation</h2>
+                <li>
+                    <Link href={'/login'}>Login</Link>
+                </li>
+                <li>
+                    <Link href={'/login/loginStudents'}>Login Students</Link>
+                </li>
+                <li>
+                    <Link href={'/login/loginTeacher'}>Login Teacher</Link>
+                </li>
+        </ul>:<Link href={'/login'}>Go to main login page</Link>
+        }
         <hr />
         {children}
     </div>
